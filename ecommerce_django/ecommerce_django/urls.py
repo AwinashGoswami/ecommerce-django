fe9a8e8c8056d81dd4c59f6988e24154cd6685ea
awinash <awinashgoswami3@gmail.com>
2023-09-06 16:46:21 +0500

    signup function created

--- ecommerce_django/ecommerce_django/urls.py
+++ ecommerce_django/ecommerce_django/urls.py
@@ -1,7 +1,7 @@
 
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from .import views
 from django.conf.urls.static import static
 
@@ -9,5 +9,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('master/', views.Master, name='master'),
-    path('', views.Index, name='index')
+    path('', views.Index, name='index'),
+    path('signup', views.singup, name="signup"),
+    path('accounts/', include('django.contrib.auth.urls'))
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- ecommerce_django/ecommerce_django/views.py
+++ ecommerce_django/ecommerce_django/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from app.models import Category, Product
+from django.contrib.auth import authenticate, login
+from app.models import UserCreateForm
 
 def Master(request):
     return render(request, 'master.html')
@@ -10,7 +12,7 @@ def Index(request):
 
     categoryID = request.GET.get('category')
     if categoryID:
-        product = Product.objects.filter(sub_category=categoryID)
+        product = Product.objects.filter(sub_category=categoryID).order_by('-id')
     else:
         product = Product.objects.all()
 
@@ -20,3 +22,23 @@ def Index(request):
     }
 
     return render(request, 'index.html', context)
+
+def singup(request):
+    if request.method == 'POST':
+        form = UserCreateForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            new_user = authenticate(
+                username = form.cleaned_data['username'],
+                password = form.cleaned_data['password1']
+            )
+            login(request, new_user)
+            return redirect('index')
+    else:
+            form = UserCreateForm()
+
+    context = {
+        'form':form
+    }
+
+    return render(request, 'registration/signup.html', context)
