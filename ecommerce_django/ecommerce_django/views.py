963bf8dc93d5451a265f7c23e59eca86a3b4a444
awinash <awinashgoswami3@gmail.com>
2023-09-06 14:09:41 +0500

    applied filter for products

--- ecommerce_django/ecommerce_django/views.py
+++ ecommerce_django/ecommerce_django/views.py
@@ -8,6 +8,12 @@ def Index(request):
     category = Category.objects.all()
     product = Product.objects.all()
 
+    categoryID = request.GET.get('category')
+    if categoryID:
+        product = Product.objects.filter(sub_category=categoryID)
+    else:
+        product = Product.objects.all()
+
     context = {
         'category': category,
         'product':product
